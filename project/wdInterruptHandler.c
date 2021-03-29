8a61511c4cfc56399d41657f44086eda45a402a6
bavalos6 <bavalos6@miners.utep.edu>
2021-03-29 10:38:36 -0600

    State Machines for leds, switches for handling interrupts, and wdInterruptH

---
+++ project/wdInterruptHandler.c
@@ -0,0 +1,17 @@
+#include <msp430.h>
+#include "led_assembly.h"
+#include "stateMachineBuzz.h"
+#include "buzzer.h"
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static char blink_count = 0;
+  if (++blink_count == 50 && (i <= 17 )) {
+    buzzer_play_sound();
+    i++;
+    blink_count = 0;
+  }
+  if(i<17 && i>0){
+    dim();
+  }
+}
